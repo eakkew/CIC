a84106071e9168bac0609ca4b79b1a359533339e
SetterTh <thanawatvarintree@gmail.com>
2013-11-06 15:41:53 +0700

    Chang formMain topmost to false

--- Caller/CIC30/CIC/FormMain.Designer.cs
+++ Caller/CIC30/CIC/FormMain.Designer.cs
@@ -993,7 +993,6 @@
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormMain";
-            this.TopMost = true;
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel2.ResumeLayout(false);
